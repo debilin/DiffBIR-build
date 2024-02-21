c91d4f61d1a2296c88ed63cb76868d158b259714
debilin <0x1e51A1363afeE8D8b82130DfB181d9fF1a151547@hashmail.dev>
2024-02-21 10:44:21 -0800

    Remove face_aligned.sh and face_unaligned.sh

--- face_aligned.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-# for aligned face inputs
-python inference_face.py \
---input inputs/ \
---sr_scale 1 \
---output results/demo/face/aligned \
---has_aligned \
---device cuda
--- face_unaligned.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-# for unaligned face inputs
-python inference_face.py \
---input inputs/ \
---sr_scale 2 \
---output results/demo/face/whole_img \
---bg_upsampler DiffBIR \
---device cuda
